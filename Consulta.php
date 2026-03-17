67ac827ac1c6add0f61fe33c047872e3e9237cff
eduardobj1905 <eduardobernal1502@gmail.com>
2026-03-17 12:35:03 +0100

    Mostrar la factura con id

--- Consulta.php
+++ Consulta.php
@@ -1,8 +1,17 @@
 <?php
 $facturas = json_decode(file_get_contents("facturas_guardadas/facturas_totales.json"), true);
+
 $id = $_GET['id'] ?? '';
 $tipoUsuario = $_GET['tipoUsuario'] ?? ''; 
-$campo = $_GET['campo'] ?? '';     
-if(!isset($facturas[$id])) exit("Factura no encontrada");
-echo $facturas[$id][$tipoUsuario][$campo];
+$campo = $_GET['campo'] ?? '';
+if (!isset($facturas[$id])) {
+    exit("Error: La factura con ID $id no existe.");
+}
+if ($tipoUsuario && $campo) {
+    echo $facturas[$id][$tipoUsuario][$campo] ?? "Campo no encontrado";
+} else {
+   echo "<pre>";
+    echo json_encode($facturas[$id], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    echo "</pre>";
+}
 ?>
