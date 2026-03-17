6f9e102c6264f5b9dc38d5dc754c3b244ca0ccb3
eduardobj1905 <eduardobernal1502@gmail.com>
2026-03-17 10:46:36 +0100

    Consultas solo un if

--- Consulta.php
+++ Consulta.php
@@ -1,10 +1,8 @@
 <?php
 $facturas = json_decode(file_get_contents("facturas_guardadas/facturas_totales.json"), true);
 $id = $_GET['id'] ?? '';
-$campo = $_GET['campo'] ?? '';
-if (isset($facturas[$id]['emisor'][$campo])) {
-   echo  $facturas[$id]['emisor'][$campo];
-} else {
-    echo "Error: No se encontró el $campo para la factura con ID $id.";
-}
+$tipoUsuario = $_GET['tipoUsuario'] ?? ''; 
+$campo = $_GET['campo'] ?? '';     
+if(!isset($facturas[$id])) echo("Factura o dato no encontrado");
+echo $facturas[$id][$tipoUsuario][$campo];
 ?>
