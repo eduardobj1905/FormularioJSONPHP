c675a6a264e386e706cdc296ff2f7a77195ac0fb
eduardobj1905 <eduardobernal1502@gmail.com>
2026-03-17 12:35:34 +0100

    Update Consulta.php

--- Consulta.php
+++ Consulta.php
@@ -1,6 +1,5 @@
 <?php
 $facturas = json_decode(file_get_contents("facturas_guardadas/facturas_totales.json"), true);
-
 $id = $_GET['id'] ?? '';
 $tipoUsuario = $_GET['tipoUsuario'] ?? ''; 
 $campo = $_GET['campo'] ?? '';
